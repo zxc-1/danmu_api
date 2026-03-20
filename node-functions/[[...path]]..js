e29c9848bc854c8fe672bb0c12c1b678af872ecd
Celestials316 <lilixu254992@gmail.com>
2026-03-20 21:42:10 +0800

    修复 EdgeOne token 路由 match 405 (#248)

--- node-functions/[[...path]]..js
+++
@@ -1,3 +0,0 @@
-import { onRequest } from './index.js'; // 改为 onRequest，支持所有方法
-
-export { onRequest }; // 复用 index.js 的 onRequest
---
+++ node-functions/api/[[default]].js
@@ -0,0 +1,3 @@
+import { onApiRequest } from '../api-handler.js';
+
+export const onRequest = onApiRequest;
