f19a5d9c4cc293708a0074f0627d542d1c4f5cc4
haode <1730142004@qq.com>
2026-08-01 12:55:27 +0800

    feat: bangumi、episode 接口返回数据新增 url 字段 (#403)
    * Add files via upload
    
    * Add files via upload

--- danmu_api/apis/dandan-api.js
+++ danmu_api/apis/dandan-api.js
@@ -1901,7 +1901,8 @@ export async function searchEpisodes(url) {
           typeDescription: animeItem.typeDescription,
           episodes: filteredEpisodes.map(ep => ({
             episodeId: ep.episodeId,
-            episodeTitle: ep.episodeTitle
+            episodeTitle: ep.episodeTitle,
+            url: ep.url || ""
           }))
         }));
       }
@@ -1948,6 +1949,7 @@ function buildBangumiData(anime, idParam = "") {
       episodeTitle: `${link.title}`,
       episodeNumber: `${i+1}`,
       airDate: anime.startDate,
+      url: link.url || ""
     });
   }
 
